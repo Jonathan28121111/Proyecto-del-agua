6ba4d2ea61f0be911295f9c940362e5bb29c17e3
Jonathan28121111 <Jonathan120428@gmail.com>
2025-10-06 13:40:09 -0700

    Primero definimos los nombres de nuestro proyecto en este caso "agua atrapada"

--- lib/proyecto_agua.dart
+++ lib/proyecto_agua.dart
@@ -1,1 +1,11 @@
-
+int aguaAtrapada(List<int> bloques) {
+  if (bloques.length < 3) {
+    return 0;
+  }
+ 
+  int n = bloques.length;
+  int aguaTotal = 0;
+ 
+  List<int> maxIzquierda = List.filled(n, 0);
+  List<int> maxDerecha = List.filled(n, 0);
+}
