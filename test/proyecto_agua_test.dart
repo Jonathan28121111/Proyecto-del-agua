bc412ad0d426aeefc5d149d524fdbd95ed423f30
Jonathan28121111 <Jonathan120428@gmail.com>
2025-10-06 13:43:50 -0700

    Lo primero que hicimos fue hacer diferentes tipos de test y los fuimos probando con la funcion dart test

--- test/proyecto_agua_test.dart
+++ test/proyecto_agua_test.dart
@@ -2,4 +2,15 @@ import 'package:proyecto_agua/proyecto_agua.dart';
 import 'package:test/test.dart';
 
 void main() {
+  test('Ejemplo del enunciado [6, 0, 3, 4, 1, 3] = 7', () {
+    expect(aguaAtrapada([6, 0, 3, 4, 1, 3]), equals(7));
+  });
+ 
+  test('Ejemplo [0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1] = 6', () {
+    expect(aguaAtrapada([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1]), equals(6));
+  });
+ 
+  test('Ejemplo [4, 2, 0, 3, 2, 5] = 9', () {
+    expect(aguaAtrapada([4, 2, 0, 3, 2, 5]), equals(9));
+  });
 }
