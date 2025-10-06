8e25dd7da125e8a3ea9826746f1d9e990541c2fd
Jonathan28121111 <Jonathan120428@gmail.com>
2025-10-06 13:31:49 -0700

    Creacion del proyecto del agua

---
+++ bin/proyecto_agua.dart
@@ -0,0 +1,5 @@
+import 'package:proyecto_agua/proyecto_agua.dart' as proyecto_agua;
+
+void main(List<String> arguments) {
+  print('Hello world: ${proyecto_agua.calculate()}!');
+}
---
+++ test/proyecto_agua_test.dart
@@ -0,0 +1,8 @@
+import 'package:proyecto_agua/proyecto_agua.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
